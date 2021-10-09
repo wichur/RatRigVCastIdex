2c229e3566773026e056e63cbfdab21767d08f0d
Maciej Raczynski <maciej.raczynski@gmail.com>
2021-10-09 02:00:17 +0200

    added upload script

--- script/upload.cmd
+++ script/upload.cmd
@@ -1,1 +1,6 @@
-..\bin\RepRapFileManager\rfm.exe backup -device czardasz -domain czardasz.local -port 80 ../src/macros 0:/macros -removeLocal
+..\bin\RepRapFileManager\rfm.exe upload	-device czardasz -domain czardasz.local -port 80 ../src/sys 0:/sys
+..\bin\RepRapFileManager\rfm.exe upload	-device czardasz -domain czardasz.local -port 80 ../src/macros 0:/macros
+..\bin\RepRapFileManager\rfm.exe upload	-device czardasz -domain czardasz.local -port 80 ../src/filaments 0:/filaments
+..\bin\RepRapFileManager\rfm.exe upload	-device czardasz -domain czardasz.local -port 80 ../src/firmware 0:/firmware
+..\bin\RepRapFileManager\rfm.exe upload	-device czardasz -domain czardasz.local -port 80 ../src/overlays 0:/overlays
+..\bin\RepRapFileManager\rfm.exe upload	-device czardasz -domain czardasz.local -port 80 ../src/www 0:/www
